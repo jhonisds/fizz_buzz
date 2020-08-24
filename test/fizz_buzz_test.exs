5c84c04d651fb8c8d6656af3bf9bef7aea58218a
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-08-24 18:01:45 -0300

    add tests fizzBuzz.

--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -2,7 +2,15 @@ defmodule FizzBuzzTest do
   use ExUnit.Case
   doctest FizzBuzz
 
-  test "greets the world" do
-    assert FizzBuzz.hello() == :world
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      expected_response = {:ok, [1, 2, :fizz, 4, :buzz, :buzz, :fizzbuzz, :buzz]}
+      assert expected_response == FizzBuzz.build("numbers.txt")
+    end
+
+    test "when a invalid file is provided, returns the converted list" do
+      expected_response = {:error, "Error read file: enoent"}
+      assert expected_response == FizzBuzz.build("invalid.txt")
+    end
   end
 end
